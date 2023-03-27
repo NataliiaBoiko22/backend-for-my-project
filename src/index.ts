0316df4036fafe84e526fb31eb543f0dacb503f2
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-03-27 22:24:53 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import * as serverService from './services/server.service'
     await mongoose.connect(
       // 'mongodb+srv://nataboykokras:qtV1xEoap0fZvxQB@cluster0.scg8pkw.mongodb.net/?retryWrites=true&w=majority'
     
-      'mongodb+srv://boikonataliia37:QBq9ISVVy0NAgkno@cluster0.ryxizcx.mongodb.net/test'
+      'mongodb+srv://boikonataliia37:QBq9lSVVy0NAgkno@cluster0.ryxizcx.mongodb.net/test'
       )
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...')
