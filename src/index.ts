0a052c2bd742460a35285962d880e44d47800270
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-09-11 18:52:00 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,13 @@ import mongoose from 'mongoose'
 import { PORT } from './constants'
 
 import * as serverService from './services/server.service';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 (async () => {
   try {
-    if (!process.env.MONGODB_URI) {
-      throw new Error('MONGODB_URI is not defined in the environment variables');
-    }
     await mongoose.connect(
       // 'mongodb+srv://nataboykokras:qtV1xEoap0fZvxQB@cluster0.scg8pkw.mongodb.net/?retryWrites=true&w=majority'
     
-      // 'mongodb+srv://boikonataliia37:QBq9lSVVy0NAgkno@cluster0.ryxizcx.mongodb.net/test'
-      process.env.MONGODB_URI
+      'mongodb+srv://boikonataliia37:QBq9lSVVy0NAgkno@cluster0.ryxizcx.mongodb.net/test'
       )
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...')
